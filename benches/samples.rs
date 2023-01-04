fb1a9a85dd95f51601f9f48e3c60cf6b56b0007f
Defmc <defmc0101@gmail.com>
2023-01-04 02:11:48 -0300

    feat(example): assert outputs

--- benches/samples.rs
+++ benches/samples.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::{collections::HashSet, time::Duration};
 
 use hermes::{BenchSize, Bencher, IterBench};
 
@@ -20,18 +20,24 @@ fn main() {
         .flatten()
         .collect();
 
+    let anwsers: HashSet<u64> = iter.iter().map(|(b, e)| pow(*b, *e)).collect();
+
     let inputs = &iter.iter().cycle();
 
     let size = BenchSize::Time(Duration::from_secs(1));
 
+    let assert_exists = |x| assert!(anwsers.contains(&x));
+
     let mut rec_bench =
         IterBench::new(inputs.clone().copied(), &|(b, e): (u64, u32)| rec_pow(b, e))
             .with_name("recursive pow function")
-            .with_size(size);
+            .with_size(size)
+            .with_post(&assert_exists);
 
     let mut linear_bench = IterBench::new(inputs.clone().copied(), &|(b, e): (u64, u32)| pow(b, e))
         .with_name("linear pow function")
-        .with_size(size);
+        .with_size(size)
+        .with_post(&assert_exists);
 
     rec_bench.run();
     linear_bench.run();
